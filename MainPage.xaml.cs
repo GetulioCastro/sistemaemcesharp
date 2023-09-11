06c26d00928bd21e3ec24cbc5d3d1e9d2eeed1c8
Getulio Castro <augustoge71@gmail.com>
2023-09-11 17:35:11 -0300

    Adicionar arquivos de projeto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace sistemaemcesharp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
